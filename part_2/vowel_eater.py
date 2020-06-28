cf26693acac0d0cc836d73149e8047665208ed0b
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-28 11:46:30 +0800

    The continue statement - the Great Vowel Eater

---
+++ part_2/vowel_eater.py
@@ -0,0 +1,14 @@
+word = input("Enter your word:")
+for letter in word:
+    if letter == "a":
+        continue
+    elif letter == "e":
+        continue
+    elif letter == "i":
+        continue
+    elif letter == "o":
+        continue
+    elif letter == "u":
+        continue
+    else:
+        print(letter)
