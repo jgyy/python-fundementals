6bc99fe513c154543cdb63f228105438416ae421
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-28 11:03:05 +0800

    assigning variables

---
+++ part_1/variables.py
@@ -0,0 +1,18 @@
+# assigning values to variables
+John = 3
+Mary = 5
+Adam = 6
+
+# printing variables on one line
+print(John, Mary, Adam, sep=",")
+
+# adding variables and printing the result
+print(John+Mary+Adam)
+
+# creating the variable Total_Apples
+Total_Apples = John + Mary + Adam
+print(Total_Apples)
+
+# Experimenting
+print(Total_Apples / 2, Total_Apples // 2, Total_Apples ** 2, Total_Apples - Mary)
+print("Total number of apples:", Total_Apples)
