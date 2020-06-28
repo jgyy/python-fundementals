fe147993d5e4dcd368dbed6566c1227933b1f5fe
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-28 14:03:25 +0800

    Writing and using your own functions - basics part 3

--- part_3/func_basic.py
+++ part_3/func_basic.py
@@ -2,12 +2,20 @@ def is_year_leap(year):
     return year % 4 == 0
 
 def days_in_month(year,month):
-    leap = 1 if year % 4 == 0 else 0
+    leap = 1 if is_year_leap(year) else 0
     dim = {
         1: 31, 2: 28 + leap, 3: 31, 4: 30, 5: 31, 6: 30,
         7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31
     }
     return dim.get(month)
+
+def day_of_year(year,month,day):
+    days = day
+    for i in range(1, month):
+        days += days_in_month(year, i)
+    return days
+
+print(day_of_year(2000,12,31))
 
 testyears = [1900, 2000, 2016, 1987]
 testmonths = [ 2, 2, 1, 11]
